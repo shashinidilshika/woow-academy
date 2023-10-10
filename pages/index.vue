05e9bda21553ca48cc52cc59066a5da07eb1696b
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-10 14:09:24 +0530

    chore(page) : example index page for members

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello Guys! Let's begin to develop </h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  height: 100vh;
+}
+
+h1 {
+  margin: 0;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+}
+</style>
