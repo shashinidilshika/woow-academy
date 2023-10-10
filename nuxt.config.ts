aa8cef317378bf1d0e93ea120bfd0fc14bf028a7
MadhushaPrasad <madushaprasad21@gmail.com>
2023-10-10 14:48:13 +0530

    chore(project) : tailwind css config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
-    devtools: {enabled: true}
+    devtools: {enabled: true},
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    // @ts-ignore
+    css: [
+        '@/assets/css/tailwind.css',
+    ]
 })
