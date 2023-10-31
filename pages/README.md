8e3b53bccafb260456adef12d6f348e9cb20b317
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2023-10-31 05:59:04 +0530

    Header and Footer (#15)
    * feat(component): footer section done
    
    * feat(component): footer section done
    
    * style(component): footer section
    
    * chore(layout): default layout
    
    * chore(component): about hero section

--- pages/README.md
+++
@@ -1,6 +0,0 @@
-# Pages Directory
-
-Nuxt provides a file-based routing to create routes within your web application using Vue Router under the hood.
-
-This directory is optional, meaning that `vue-router` won't be included if you only use app.vue (unless you set `pages:
-true` in `nuxt.config` or have a `app/router.options.ts`), reducing your application's bundle size.
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,39 @@ module.exports = {
   theme: {
     extend: {
       colors: {},
+      height: {
+        '35vh': '35vh',
+        '84vh-32': 'calc(84vh - 32px)',
+        '84vh': '84vh',
+        '85vh-50': 'calc(85vh - 50px)',
+        '85vh': '85vh',
+        '95vh': '95vh',
+        '15vw': '15vw',
+        '20vw': '20vw',
+      },
+      width: {
+        27: '27px',
+        '15vw': '15vw',
+        '20vw': '20vw',
+      },
+      minHeight: {
+        '84vh': '84vh',
+        '85vh': '85vh',
+        '90vh': '90vh',
+      },
+      maxHeight: {
+        '35vh': '35vh',
+        '84vh-50': 'calc(84vh - 50px)',
+        '95vh': '95vh',
+        '65vh': '65vh',
+      },
       screens: {
         mobile: { max: '639px' },
       },
       fontFamily: {
-        body: ['Open Sans'],
+        body: ['Poppins'],
         sans: ['Open Sans'],
+        poppins: ['Poppins'],
       },
     },
   },
